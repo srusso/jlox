cd368571c2dc8bfd4722f7acb5a648d3b2458f36
Simone <s@sr89.net>
2023-10-03 22:24:06 +0200

    Add some basic methods to Lox

--- src/main/java/net/sr89/jlox/Lox.java
+++ src/main/java/net/sr89/jlox/Lox.java
@@ -1,7 +1,56 @@
 package net.sr89.jlox;
 
-public class Lox {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+public class Lox {
+    // see https://man.openbsd.org/sysexits
+    private final static int EX_USAGE = 64;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(EX_USAGE);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        run(
+            Files.readString(Paths.get(path), Charset.defaultCharset())
+        );
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        System.out.println("Welxome to the jlox REPL.");
+        System.out.println("Exit with Control-D");
+
+        while (true) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) {
+                break;
+            }
+            run(line);
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
     }
 }

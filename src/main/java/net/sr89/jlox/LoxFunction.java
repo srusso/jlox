1d7e6e82c6c63a0661d14f2da628a438dd8d9b9e
Simone <s@sr89.net>
2023-11-26 12:21:21 +0100

    Functions now operate in the scope of where they were defined. Their direct parent environment isn't the "global" environment anymore.

--- src/main/java/net/sr89/jlox/LoxFunction.java
+++ src/main/java/net/sr89/jlox/LoxFunction.java
@@ -4,8 +4,15 @@ import java.util.List;
 
 class LoxFunction implements LoxCallable {
   private final Stmt.Function declaration;
-  LoxFunction(Stmt.Function declaration) {
+
+  /**
+   * This is the environment that is active when the function is <i>declared</i>, not called.
+   */
+  private final Environment closure;
+
+  LoxFunction(Stmt.Function declaration, Environment closure) {
     this.declaration = declaration;
+    this.closure = closure;
   }
 
   @Override
@@ -15,7 +22,7 @@ class LoxFunction implements LoxCallable {
 
   @Override
   public Object call(Interpreter interpreter, List<Object> arguments) {
-    Environment environment = new Environment(interpreter.globals);
+    Environment environment = new Environment(closure);
     for (int i = 0; i < declaration.params.size(); i++) {
       environment.define(declaration.params.get(i).lexeme, arguments.get(i));
     }
